6b4f52b7d0b6dc6b85181431e23fe7f68c3008a9
andreikhanau <andreikhanau@yahoo.com>
2021-02-09 15:37:38 +0100

    fixing

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,20 +9,20 @@ const extractData = (pathToFile) => fs.readFileSync(pathToFile, 'utf-8');
 const getFilePath = (fileName) => path.join(__dirname, '..', '__fixtures__', fileName);
 const getTestResult = (resultFileName) => extractData(getFilePath(resultFileName));
 test('gendiff stylish format test for .json file', () => {
-  expect(gendiff('file1.json', 'file2.json', 'stylish')).toBe(getTestResult('stylishResult'));
+  expect(gendiff(getFilePath('file1.json'), getFilePath('file2.json'), 'stylish')).toBe(getTestResult('stylishResult'));
 });
 test('gendiff plain format test for .json file', () => {
-  expect(gendiff('file1.json', 'file2.json', 'plain')).toBe(getTestResult('plainResult'));
+  expect(gendiff(getFilePath('file1.json'), getFilePath('file2.json'), 'plain')).toBe(getTestResult('plainResult'));
 });
 test('gendiff json format test for .json file', () => {
-  expect(gendiff('file1.json', 'file2.json', 'json')).toBe(getTestResult('jsonResult'));
+  expect(gendiff(getFilePath('file1.json'), getFilePath('file2.json'), 'json')).toBe(getTestResult('jsonResult'));
 });
 test('gendiff stylish format test for .yml file', () => {
-  expect(gendiff('file1.yml', 'file2.yml', 'stylish')).toBe(getTestResult('stylishResult'));
+  expect(gendiff(getFilePath('file1.yml'), getFilePath('file2.yml'), 'stylish')).toBe(getTestResult('stylishResult'));
 });
 test('gendiff plain format test for .yml file', () => {
-  expect(gendiff('file1.yml', 'file2.yml', 'plain')).toBe(getTestResult('plainResult'));
+  expect(gendiff(getFilePath('file1.yml'), getFilePath('file2.yml'), 'plain')).toBe(getTestResult('plainResult'));
 });
 test('gendiff json format test for .yml file', () => {
-  expect(gendiff('file1.yml', 'file2.yml', 'json')).toBe(getTestResult('jsonResult'));
+  expect(gendiff(getFilePath('file1.yml'), getFilePath('file2.yml'), 'json')).toBe(getTestResult('jsonResult'));
 });
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
 import fs from 'fs';
 import parseTheFile from './parsers.js';
 import render from './formatters/index.js';
 import makeDiff from './makeDiff.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const getFilePath = (fileName) => path.join(__dirname, '..', '__fixtures__', fileName);
 const extractData = (file) => fs.readFileSync(file, 'utf-8');
 const gendiff = (pathToFile1, pathToFile2, outputFormat = 'stylish') => {
   const extractFile1 = extractData(pathToFile1);
