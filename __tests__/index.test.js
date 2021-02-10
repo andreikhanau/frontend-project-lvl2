bde4528288282cd64aa4057cceefe8c7db999d66
andreikhanau <andreikhanau@yahoo.com>
2021-02-10 11:56:33 +0100

    fixing

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
-import gendiff from '../src/index';
+import gendiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
