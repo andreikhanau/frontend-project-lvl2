735b66c904123c7c5c4c21536f80d59155ae226b
andreikhanau <andreikhanau@yahoo.com>
2021-02-16 15:04:54 +0100

    final fix

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import fs from 'fs';
+import path from 'path';
 import parseTheFile from './parsers.js';
 import render from './formatters/index.js';
 import makeDiff from './makeDiff.js';
 
 const extractData = (file) => fs.readFileSync(file, 'utf-8');
 const gendiff = (pathToFile1, pathToFile2, outputFormat = 'stylish') => {
-  const extractFile1 = extractData(pathToFile1);
-  const extractFile2 = extractData(pathToFile2);
+  const extractFile1 = extractData(path.resolve(pathToFile1));
+  const extractFile2 = extractData(path.resolve(pathToFile2));
   const file1 = parseTheFile(pathToFile1)(extractFile1);
   const file2 = parseTheFile(pathToFile2)(extractFile2);
   const diff = makeDiff(file1, file2);
